b44b61c8c0b3ac5ca017d6edcb50ce383f9cfb5b
Allisson <allissonsampaio98@gmail.com>
2019-01-07 03:06:48 -0200

    Busca de produtos por nome e categorias

--- src/main/java/com/example/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/example/cursomc/repositories/ProdutoRepository.java
@@ -1,10 +1,24 @@
 package com.example.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.example.cursomc.domain.Categoria;
 import com.example.cursomc.domain.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+    @Transactional(readOnly = true)
+    @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+    Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+
+    //Método abaixo faz a mesma coisa do que a de cima, porém usando o padrão do spring data sem passar o JPQL na mão. NICE.
+    //Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
